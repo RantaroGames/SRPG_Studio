a7603be0207dfcbe819ac111f61517b463cd628f
RantaroGames <96756070+RantaroGames@users.noreply.github.com>
2022-06-01 15:39:10 +0900

    Update AbilityCalculator_getAgility_mod.js

--- 計算式変更_戦速/AbilityCalculator_getAgility_mod.js
+++ 計算式変更_戦速/AbilityCalculator_getAgility_mod.js
@@ -21,6 +21,9 @@ AbilityCalculator.getAgilityの処理をコメントアウト/解除してくだ
 ■作成者
 ran
 
+■利用規約
+https://github.com/RantaroGames/SRPG_Studio/blob/be1b84ab349a0ac1a3573bf645e5c78cb3ab12c3/README.md
+
 ■更新履歴
 2022/01/27 新規作成
 
