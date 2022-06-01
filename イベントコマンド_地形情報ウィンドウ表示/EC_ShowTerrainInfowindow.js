842cb25f5336bb9df02b0dee9aebb58af3e36067
RantaroGames <96756070+RantaroGames@users.noreply.github.com>
2022-06-01 16:18:11 +0900

    Update EC_ShowTerrainInfowindow.js

--- イベントコマンド_地形情報ウィンドウ表示/EC_ShowTerrainInfowindow.js
+++ イベントコマンド_地形情報ウィンドウ表示/EC_ShowTerrainInfowindow.js
@@ -38,6 +38,9 @@ OriginalEC_ShowTerrainInfoWindow
 ■作成者
 ran
 
+■利用規約
+https://github.com/RantaroGames/SRPG_Studio/blob/be1b84ab349a0ac1a3573bf645e5c78cb3ab12c3/README.md
+
 ■更新履歴
 2021/12/22 新規作成
 
