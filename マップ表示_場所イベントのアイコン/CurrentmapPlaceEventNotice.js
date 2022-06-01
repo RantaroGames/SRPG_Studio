b04470c5bfd66f5f67edbec7b291535dcfef67b1
RantaroGames <96756070+RantaroGames@users.noreply.github.com>
2022-06-01 15:43:22 +0900

    Update CurrentmapPlaceEventNotice.js

--- マップ表示_場所イベントのアイコン/CurrentmapPlaceEventNotice.js
+++ マップ表示_場所イベントのアイコン/CurrentmapPlaceEventNotice.js
@@ -17,6 +17,9 @@ CurrentmapPlaceEventNotice.js
 ■作成者
 ran
 
+■利用規約
+https://github.com/RantaroGames/SRPG_Studio/blob/be1b84ab349a0ac1a3573bf645e5c78cb3ab12c3/README.md
+
 ■更新履歴
 2022/04/10 プラグインの説明文を追記
 
