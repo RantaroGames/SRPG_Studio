a2b743627169df236cdfb02b21ef978cc57232b7
RantaroGames <96756070+RantaroGames@users.noreply.github.com>
2022-06-01 16:14:00 +0900

    Update window-unitsentence_add.js

--- ウィンドウ表示_武器の属性アイコン表示/window-unitsentence_add.js
+++ ウィンドウ表示_武器の属性アイコン表示/window-unitsentence_add.js
@@ -14,6 +14,9 @@ window-unitsentence_add.js
 ■作成者
 ran
 
+■利用規約
+https://github.com/RantaroGames/SRPG_Studio/blob/be1b84ab349a0ac1a3573bf645e5c78cb3ab12c3/README.md
+
 ■更新履歴
 2021/12/01 新規作成
 2021/12/14　iconhandleの取得をdrawメソッドではなく、setCalculatorValueメソッドで取得する処理に変更
