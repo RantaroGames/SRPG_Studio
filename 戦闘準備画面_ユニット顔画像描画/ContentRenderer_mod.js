83b0bfa5d73fed34065a7541dfe86b945d3d1149
RantaroGames <96756070+RantaroGames@users.noreply.github.com>
2022-06-04 08:14:34 +0900

    Update ContentRenderer_mod.js

--- 戦闘準備画面_ユニット顔画像描画/ContentRenderer_mod.js
+++ 戦闘準備画面_ユニット顔画像描画/ContentRenderer_mod.js
@@ -24,45 +24,58 @@ https://github.com/RantaroGames/SRPG_Studio/blob/be1b84ab349a0ac1a3573bf645e5c78
 
 
 (function() {
+	
+//-------------------------------------------------
+// 表示設定
+//-------------------------------------------------
+//戦闘準備画面でキャラチップも表示する場合は、true
+var CharaChipisDraw = false;
+
 
 //-------------------------------------------------
 // 顔画像を任意のサイズで描画する処理
 // 素材規格の比率は1：1なのでサイズはヨコタテ同じ長さで描画する
 //-------------------------------------------------
-ContentRenderer.drawStretchUnitFace = function(x, y, unit, isReverse, alpha, size) {
-	var handle = unit.getFaceResourceHandle();
-	var pic = GraphicsRenderer.getGraphics(handle, GraphicsType.FACE);
+var ContentRenderer_Mod = {
+
+	drawStretchUnitFace: function(x, y, unit, isReverse, alpha, size) {
+		var handle = unit.getFaceResourceHandle();
+		var pic = GraphicsRenderer.getGraphics(handle, GraphicsType.FACE);
+		
+		if (pic === null) {
+			return;
+		}
+		
+		pic.setReverse(isReverse);
+		pic.setAlpha(alpha);
 	
-	if (pic === null) {
-		return;
+		this._drawShrinkFace(x, y, handle, pic, size);
+	},
+
+	_drawShrinkFace: function(xDest, yDest, handle, pic, size) {
+		var xSrc, ySrc;
+		var destWidth = size;//GraphicsFormat.FACE_WIDTH;
+		var destHeight = size;//GraphicsFormat.FACE_HEIGHT;
+		var srcWidth = GraphicsFormat.FACE_WIDTH;//destWidth;
+		var srcHeight = GraphicsFormat.FACE_HEIGHT;//destHeight;
+	
+		if (root.isLargeFaceUse() && pic.isLargeImage()) {
+			srcWidth = root.getLargeFaceWidth();
+			srcHeight = root.getLargeFaceHeight();
+		}
+	
+		xSrc = handle.getSrcX() * srcWidth;
+		ySrc = handle.getSrcY() * srcHeight;
+		pic.drawStretchParts(xDest, yDest, destWidth, destHeight, xSrc, ySrc, srcWidth, srcHeight);
 	}
-	
-	pic.setReverse(isReverse);
-	pic.setAlpha(alpha);
-	
-	this._drawShrinkFace2(x, y, handle, pic, size);
-};
+}
 
-ContentRenderer._drawShrinkFace2 = function(xDest, yDest, handle, pic, size) {
-	var xSrc, ySrc;
-	var destWidth = size;//GraphicsFormat.FACE_WIDTH;
-	var destHeight = size;//GraphicsFormat.FACE_HEIGHT;
-	var srcWidth = GraphicsFormat.FACE_WIDTH;//destWidth;
-	var srcHeight = GraphicsFormat.FACE_HEIGHT;//destHeight;
-
-	if (root.isLargeFaceUse() && pic.isLargeImage()) {
-		srcWidth = root.getLargeFaceWidth();
-		srcHeight = root.getLargeFaceHeight();
-	}
-	
-	xSrc = handle.getSrcX() * srcWidth;
-	ySrc = handle.getSrcY() * srcHeight;
-	pic.drawStretchParts(xDest, yDest, destWidth, destHeight, xSrc, ySrc, srcWidth, srcHeight);
-};
-
+// 戦闘準備画面でユニット一覧に顔画像を表示する処理
+//　キャラチップも表示したい場合は、本コード内(32行付近)のCharaChipisDrawの値をtrueに変更してください
 
 // プラグインの競合が発生している場合、本プラグインのUnitSelectScrollbar.drawScrollContentオブジェクトをコメントアウトして
 // UnitSelectScrollbar.drawScrollContentを変更しているプラグインにマージしてください。
+// ↓ ここからコメントアウト
 UnitSelectScrollbar.drawScrollContent = function(x, y, object, isSelect, index) {
 	var range;
 	var unit = object;
@@ -84,10 +97,14 @@ UnitSelectScrollbar.drawScrollContent = function(x, y, object, isSelect, index) 
 	y += 10;
 	unitRenderParam.alpha = alpha;
 	
-	// 本来の処理（キャラチップ画像を描画する) 不要なら下記メソッドをコメントアウトしてください。
-	// キャラチップも並べて描画するためにx座標を調整している(-dxの部分を追記)
-	UnitRenderer.drawDefaultUnit(unit, x　- dx, y, unitRenderParam);
-
+	// 競合対策で記述を写す場合
+	// 該当するプラグインの「 UnitRenderer.drawDefaultUnit(unit, x, y, unitRenderParam); 」をコメントアウトして
+	// その直下に以下の部分を書き写してください
+	// ↓ ここから
+	if (CharaChipisDraw) {
+		UnitRenderer.drawDefaultUnit(unit, x　- dx, y, unitRenderParam);
+	}
+	
 	// ユニット顔画像を描画する処理
 	// 戦闘準備画面で描画するオブジェクトの高さは規定で80(UnitSelectScrollbar.getObjectHeight())
 	// 96*96だとはみ出るので縮小して描画する。
@@ -96,7 +113,8 @@ UnitSelectScrollbar.drawScrollContent = function(x, y, object, isSelect, index) 
 	var dy = -8;
 		dx = -8;
 
-	ContentRenderer.drawStretchUnitFace(x　+ dx, y + dy, unit, false, alpha, size);
+	ContentRenderer_Mod.drawStretchUnitFace(x　+ dx, y + dy, unit, false, alpha, size);
+	// ↑ ここまで
 	
 	// 名前を指定した範囲に描画する処理
 	// 下記コードの数値(50, 30)を変更すると名前を描画する範囲の開始地点を調整できます。
@@ -104,5 +122,6 @@ UnitSelectScrollbar.drawScrollContent = function(x, y, object, isSelect, index) 
 	range = createRangeObject(x - 50, y + 30, length, 40);
 	TextRenderer.drawRangeAlphaText(range, TextFormat.CENTER, unit.getName(), length, color, alpha, font);
 };
+// ↑ コメントアウトここまで
 
 })();
