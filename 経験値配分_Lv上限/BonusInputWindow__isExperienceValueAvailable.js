dc25948c56ed6e468b5d035f4a6f59dfa0e76672
RantaroGames <96756070+RantaroGames@users.noreply.github.com>
2022-07-11 15:02:37 +0900

    Update BonusInputWindow__isExperienceValueAvailable.js

--- 経験値配分_Lv上限/BonusInputWindow__isExperienceValueAvailable.js
+++ 経験値配分_Lv上限/BonusInputWindow__isExperienceValueAvailable.js
@@ -8,14 +8,12 @@ BonusInputWindow__isExperienceValueAvailable.js
 拠点の「経験値配分」でボーナスを割り振れるユニットを指定したレベルで制限します。
 ユニットのレベルが指定したレベル上限「以上の場合」は、ボーナスを割り振ることができなくなります。
 
-初期設定では、
-　下級クラスの上限値は10 (LV9のユニットまで経験値を割り振れる)
-　上級クラスの上限値は1　 (上級クラスは経験値配分でのレベルアップ不可。※CCでレベルが1に戻る仕様を想定)
-
 ■使用方法
 1.このファイルをpluginフォルダに入れる
-2.レベルキャップの値を変更したい場合は下記コード内の対応する数値を書き換えてください
-  (BonusInputWindow._getLevelCap関数の処理）
+2.エディタ上でレベルキャップの値を設定するための変数を作成する
+2-1.下記コード内の値を作成した変数に合わせて変更する
+※変数指定が不正だった場合は、table:0, index:0の値が返る
+
   
 ■作成者
 ran
@@ -28,7 +26,17 @@ https://github.com/RantaroGames/SRPG_Studio/blob/be1b84ab349a0ac1a3573bf645e5c78
 */
 
 (function() {
-	
+
+// レベルキャップの値を取得するための（エディタ上の）変数を設定する
+var LevelCapValue = {
+	// 変数テーブル(タブ)のページ数（左端から0~5。5はid変数のタブ)
+	VariablePage: 5,
+	// 下級クラスのレベルキャップ値を格納する変数のID
+	LowerClassId: 0,
+	// 上級クラスのレベルキャップ値を格納する変数のID
+	UpperClassId: 1
+};	
+
 BonusInputWindow._isLevelCapped = false;
 
 var _BonusInputWindow__isExperienceValueAvailable = BonusInputWindow._isExperienceValueAvailable;
@@ -43,13 +51,13 @@ BonusInputWindow._isExperienceValueAvailable = function() {
 };
 
 BonusInputWindow._getLevelCap = function(unit) {
-	// レベルキャップ値(0以上の整数)
-	var lvcap = 10;
+	// レベルキャップ値(指定した変数から取得する)
+	var lvcap = f_getVariable_value(LevelCapValue.VariablePage, LevelCapValue.LowerClassId);
 	var rank = unit.getClass().getClassRank();
 	
 	if (rank > 0) {
-		// 上級クラスのレベルキャップ値(0以上の整数)
-		lvcap = 1;
+		// 上級クラスのレベルキャップ値(指定した変数から取得する)
+		lvcap = f_getVariable_value(LevelCapValue.VariablePage, LevelCapValue.UpperClassId);
 	}
 	
 	// ユニットのレベルがレベルキャップ値以上の場合は経験値配分できない
@@ -67,5 +75,16 @@ BonusInputWindow._getMessage = function() {
 	
 	return StringTable.ExperienceDistribution_BonusShortage;
 };
+
+
+
+//変数指定エラー時は、table:0, index:0の値が返る
+function f_getVariable_value(page, id)
+{
+	var table = root.getMetaSession().getVariableTable(page);
+	var index = table.getVariableIndexFromId(id);
+	
+	return table.getVariable(index);
+}
 
 })();
