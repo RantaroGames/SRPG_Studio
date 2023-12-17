e5437bf7b44b8d733b96982b6aa0d25bb770f1de
RantaroGames <96756070+RantaroGames@users.noreply.github.com>
2023-12-18 01:42:51 +0900

    Update DopingItemControl_mod.js

--- Tips/DopingItemControl_mod.js
+++ Tips/DopingItemControl_mod.js
@@ -1,13 +1,12 @@
 // ver1.288で能力値が1つも上昇しない場合はドーピングアイテムが使用不可になるように仕様変更になった
-// この問題を雑に回避する手段として、経験値のみを入手するドーピングアイテムにカスタムパラメータ { expgain: true } を設定する
+// この問題を雑に回避する手段として、経験値を入手できる設定であれば強制的に使用可能にする
 
 (function() {
 
 var _DopingItemControl_isItemAllowed = DopingItemControl.isItemAllowed;
 DopingItemControl.isItemAllowed = function(targetUnit, item) {
-	if (typeof item.custom.expgain === 'boolean') {
-		return item.custom.expgain;
-	}
+	// 経験値を入手できるなら使用可能
+	if (item.getExp() > 0) return true;
 	
 	return _DopingItemControl_isItemAllowed.call(this, targetUnit, item);
 };
