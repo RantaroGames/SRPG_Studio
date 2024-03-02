5774a9150731381883d2fb3830fee10398dae4e6
RantaroGames <96756070+RantaroGames@users.noreply.github.com>
2024-03-02 23:29:07 +0900

    Update UnitSentence_Support_drawUnitFace.js

--- 支援相手_顔画像表示/UnitSentence_Support_drawUnitFace.js
+++ 支援相手_顔画像表示/UnitSentence_Support_drawUnitFace.js
@@ -2,15 +2,68 @@
 ■ファイル
 UnitSentence_Support_drawUnitFace.js
 
-■SRPG Studio対応バージョン:1.270
+■SRPG Studio対応バージョン:1.291
 
 ■プラグインの概要
 UnitSentenceWindowの「支援相手」表示欄にユニットの顔画像を縮小表示します。
+
+・機能追加
+支援ランクを表示できるようにしました。
 
 ■使用方法
 1.このファイルをpluginフォルダに入れる
 
 ※画像のサイズや表示位置を変更したい場合は下記コード内「設定項目」の該当箇所を変更してください。
+
+
+・支援ランクを文字で表示する機能用の説明
+支援ランクを表示するためにユニットのカスタムパラメータに指定する値を設定しなくてはなりません。
+
+カスタムパラメータの表記例
+{
+  supportDataArray:[ [1, 2], [3, 1] ]	
+}
+
+@ supportDataArray 配列
+配列の要素は、[ 支援相手のid, 現在のランクの数値 ]
+支援相手がプレイヤーユニット以外場合(敵同士で支援を設定しているなど)は
+エディタ上のidではなく、ゲーム内で確認できるidの値を記述してください。
+（例：エディタ上でid:0の敵ユニットのidは65536 ）
+
+ゲームレイアウト > コマンドレイアウト > ユニット概要の項目を表示させることでmap上でのidが確認できるようになります。 
+
+
+※カスタムパラメータの設定方法
+A.支援を成立させた時点でイベントコマンドを通じてカスタムパラメータを設定する
+<スクリプトの実行>を選択
+
+タブ：スクリプトの設定
+・コード実行のコードボックスに以下のメソッドを記述する
+UnitCustomParameterContorol._setArrayData(targetId, value);
+
+targetId: 支援相手になるユニットのid
+value: 支援ランクの数値
+
+タブ：オリジナルデータの設定
+ユニットの項目にカスタムパラメータを設定するユニットを指定する
+
+
+双方向の支援が成立しているならAの作業をそれぞれのユニットに対して行う
+
+
+
+B.ユニットのカスタムパラメータに事前に記述しておく
+既に支援が成立した状態で登場するユニットであれば、ユニットのカスタムパラメータに値を設定しておくことができます
+
+
+
+■競合が発生した場合は
+本プラグインでは処理の都合上、元の支援対象の描画処理を使用していません。
+他のプラグインとの競合が発生した場合は、本プラグイン内の変数をtrueにしたり
+(110行付近) var useOriginalMethod = true;
+
+プラグインのリネームやフォルダ階層移動によって読み込み順が変わることで解消される可能性があります。
+
 
 ■作成者
 ran
@@ -20,6 +73,7 @@ https://github.com/RantaroGames/SRPG_Studio/blob/be1b84ab349a0ac1a3573bf645e5c78
 
 ■更新履歴
 2022/10/23 新規作成
+2024/03/02 支援ランクを文字表示する機能を追加
 
 */
 
@@ -44,10 +98,154 @@ var UnitSentenceSpaceY = 25; //this._unitSentenceWindow.getUnitSentenceSpaceY();
 
 // センテンスウィンドウの幅を広げたい場合
 var WidthExtension = 0;
+
+//------------------------------------------
+// 支援ランク表示に関する設定
+//------------------------------------------
+// 支援ランクを文字表示する(true) / しない(false)
+var isSupportRankDisplay = true;
+
+// 支援相手の表示方法を元のスクリプトの表示を利用する(true)/しない(false)
+// 他のプラグインと競合する場合 true に設定してみてください
+var useOriginalMethod = true;
+
+// 支援ランクを表示する文字の配列
+var ConvertSupportRank = [ 'C', 'B', 'A', 'S' ];
+
+// 支援ランク表示カラー （16進数表記）
+var RankColor = [0xffffff, 0xf9f09d, 0xccff33, 0x66ff33];
+
 //------------------------------------------
 
-// 支援相手を表示するオブジェクト(オーバーライド)
+UnitSentence.Support._supportData = null;
+
+var alias_Support_setCalculatorValue = UnitSentence.Support.setCalculatorValue;
+UnitSentence.Support.setCalculatorValue = function(unit, weapon, totalStatus) {
+	alias_Support_setCalculatorValue.call(this, unit, weapon, totalStatus);
+	this._supportData = this._getSupportData(unit);
+};
+
+// ユニットが持つ支援データからオブジェクトを取得しておく
+// {targetName: '相手の名前', handle: '顔画像のhandle', rank: 支援ランクの数値}
+UnitSentence.Support._getSupportData = function(unit) {
+	var i, count, data, targetUnit, targetName, obj;
+	var supportArray = [];
+	
+	function f_getIndex(arr, name)
+	{
+		var i, data;
+		for (i = 0; i < arr.length; i++) {
+			data = arr[i];
+			if (data !== null && data.targetName === name) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	count = unit.getSupportDataCount();
+	for (i = 0; i < count; i++) {
+		obj = {targetName: null, handle: null, rank: 0};
+		data = unit.getSupportData(i);
+		targetUnit = data.getUnit();
+		
+		if (targetUnit !== null && data.isGlobalSwitchOn() && data.isVariableOn()) {
+			targetName = targetUnit.getName();
+			
+			// 支援する相手の名前が配列supportArrayに格納されていなければ支援ランクを取得する
+			if (f_getIndex(supportArray, targetName) === -1) {
+				obj.targetName = targetName;
+				obj.handle = targetUnit.getFaceResourceHandle();
+				obj.rank = this._getSupportRank(unit, targetUnit);
+
+				supportArray.push(obj);
+			}
+		}
+	}
+
+	return supportArray;
+};
+
+// ユニットのカスタムパラメータから支援相手のidに対応する支援ランクの値を取得する
+// unit.custom.supportDataArray @ [[targetId, supportRank]]
+UnitSentence.Support._getSupportRank = function(unit, targetUnit) {
+	var i, count, data, targetId;
+	var arr = unit.custom.supportDataArray;
+	
+	if (arr === null || typeof arr === 'undefined') return 0;
+
+	targatId = targetUnit.getId();
+
+	count = arr.length;
+	for (i = 0; i < count; i++) {
+		data = arr[i];
+		if (data === null) continue;
+		if (data[0] === targatId) {
+			return data[1];
+		}
+	}
+	return 0;
+};
+
+var _UnitSentence_Support_drawUnitSentence = UnitSentence.Support.drawUnitSentence;
 UnitSentence.Support.drawUnitSentence = function(x, y, unit, weapon, totalStatus) {
+	if (this._supportData !== null) {
+		if (useOriginalMethod) {
+			_UnitSentence_Support_drawUnitSentence.call(this, x + NamePosX, y, unit, weapon, totalStatus);
+		}
+		this._drawSupportData(x, y, unit, weapon, totalStatus);
+	}
+	else {
+		_UnitSentence_Support_drawUnitSentence.call(this, x, y, unit, weapon, totalStatus);
+	}
+};
+
+UnitSentence.Support._drawSupportData = function(x, y, unit, weapon, totalStatus) {
+	var arr, i, count, obj, rank, rankColor;
+	var textui = this.getUnitSentenceTextUI();
+	var color = ColorValue.KEYWORD;
+	var font = textui.getFont();
+	var length = this._getTextLength();
+	var dx = 96;
+	
+	if (!useOriginalMethod) {
+		TextRenderer.drawKeywordText(x, y, StringTable.UnitSentence_Support, length, color, font);
+	}
+	y += this._unitSentenceWindow.getUnitSentenceSpaceY();
+
+	arr = this._supportData;
+	count = arr.length;
+	
+	for (i = 0; i < count; i++) {
+		obj = arr[i];
+		if (obj === null) continue;
+				
+		// 顔画像を縮小表示する処理
+		func_drawShrinkFace(x - ImagePosX, y, obj.handle, FaceImageSize, FaceImageSize);
+
+		if (!useOriginalMethod) {
+			TextRenderer.drawKeywordText(x + NamePosX, y, obj.targetName, length, color, font);
+		}
+		
+		// 支援ランクを文字表示する場合の処理
+		if (isSupportRankDisplay) {
+			rank = ConvertSupportRank[obj.rank];
+			rankColor = RankColor[obj.rank];
+	
+			if (typeof rank !== 'undefined' && typeof rankColor !== 'undefined') {
+				TextRenderer.drawKeywordText(x + dx, y, rank, -1, rankColor, font);
+			}
+			else {
+				NumberRenderer.drawNumber(x + dx, y, obj.rank);
+			}
+		}
+
+		y += this._unitSentenceWindow.getUnitSentenceSpaceY();
+	}	
+};
+
+// 支援相手を表示するオブジェクト(オーバーライド)
+/* UnitSentence.Support.drawUnitSentence = function(x, y, unit, weapon, totalStatus) {
 	var i, count, data, targetUnit;
 	var textui = this.getUnitSentenceTextUI();
 	var color = ColorValue.KEYWORD;
@@ -73,7 +271,7 @@ UnitSentence.Support.drawUnitSentence = function(x, y, unit, weapon, totalStatus
 			y += UnitSentenceSpaceY;
 		}
 	}
-};
+}; */
 
 // (描画先のx座標, y座標, リソースハンドル, 描画先の幅, 描画先の高さ)
 function func_drawShrinkFace(xDest, yDest, handle, destWidth, destHeight)
@@ -93,7 +291,7 @@ function func_drawShrinkFace(xDest, yDest, handle, destWidth, destHeight)
 	xSrc = handle.getSrcX() * srcWidth;
 	ySrc = handle.getSrcY() * srcHeight;
 	
-	//　(描画先x, y, 拡大/縮小した幅, 高さ, 描画元のx座標(リソース上のx座標×画像タイプの幅), y座標, 描画元の幅, 高さ)
+	// (描画先x, y, 拡大/縮小した幅, 高さ, 描画元のx座標(リソース上のx座標×画像タイプの幅), y座標, 描画元の幅, 高さ)
 	pic.drawStretchParts(xDest, yDest, destWidth, destHeight, xSrc, ySrc, srcWidth, srcHeight);
 }
 
@@ -104,3 +302,141 @@ UnitSentenceWindow.getWindowWidth = function() {
 };
 
 })();
+
+
+/*
+ユニットが持つカスタムパラメータの中で指定したパラメータに任意の値を取得/設定します
+
+・カスタムパラメータを取得したい
+1.スクリプトの実行> コード実行に以下を記述する
+UnitCustomParameterContorol.getCustomParameter();
+
+2.オリジナルデータでユニットを指定し、キーワードに操作したいカスタムパラメータの名前を記述する
+  指定したユニットがnullまたはキーワードを指定しなかった場合、カスタムパラメータが存在しなかった場合は、'undefined'が返ります
+  
+・カスタムパラメータを設定したい
+1.スクリプトの実行> コード実行に以下を記述する
+UnitCustomParameterContorol.setCustomParameter(value);
+
+2.オリジナルデータでユニットを指定し、キーワードに操作したいカスタムパラメータの名前を記述する
+3.引数(value)をカスタムパラメータに設定したい値を記述する
+  value に指定する値は'Boolean', 'Number', 'String'型を推奨します
+  指定した名前のカスタムパラメータが存在しなかった場合、新たに(値と共に)設定されます
+  
+ ・オリジナルデータの数値(1~6)で取得した値をカスタムパラメータに設定したい
+1.スクリプトの実行> コード実行に以下を記述する
+UnitCustomParameterContorol.setOriginalDataNumber(index);
+
+2.オリジナルデータでユニットを指定し、キーワードに操作したいカスタムパラメータの名前を記述する
+3.引数(index)に0～5の整数を記述する(数値1~6に対応しています)
+  indexで指定した数値の値がカスタムパラメータに設定されます(indexが不正な場合は、何もせず処理を終了します)
+  指定した名前のカスタムパラメータが存在しなかった場合、新たに(値と共に)設定されます
+*/
+
+var UnitCustomParameterContorol = {
+	_getOriginalContent: function() {
+		return root.getEventCommandObject().getOriginalContent();
+	},
+	
+	_getUnit: function() {
+		return this._getOriginalContent().getUnit();
+	},
+	
+	_getKeyWord: function() {
+		return this._getOriginalContent().getCustomKeyword();
+	},
+	
+	// 数値1~6を取得する(引数indexは0~5を指定)
+	_getValue: function(index) {
+		return this._getOriginalContent().getValue(index);
+	},
+	
+	getCustomParameter: function() {
+		var unit = this._getUnit();
+		var keyword = this._getKeyWord();
+		
+		if (unit === null || keyword === '') {
+			return;
+		}
+	
+		return unit.custom[keyword];
+	},
+	
+	setCustomParameter: function(value) {
+		var unit = this._getUnit();
+		var keyword = this._getKeyWord();
+		
+		if (unit === null || keyword === '') {
+			return;
+		}
+		
+		unit.custom[keyword] = value;
+	},
+	
+	// indexで指定したオリジナルデータの数値をカスタムパラメータに設定する
+	setOriginalDataNumber: function(index) {
+		var unit = this._getUnit();
+		var keyword = this._getKeyWord();
+		var value;
+		
+		if (unit === null || keyword === '') {
+			return;
+		}
+		
+		if (typeof index !== 'number' || index < 0 || index > 5) {
+//			root.log('index には 0~5 の整数を指定してください');
+			return;
+		}
+		
+		value = this._getValue(index);
+		
+		unit.custom[keyword] = value;
+	},
+	
+	_setArrayData: function(targetId, value) {
+		var arr = this.getCustomParameter();
+		var i, count, data, index;
+		
+		if (Object.prototype.toString.call(arr) !== '[object Array]') {
+			arr = [];
+		}
+		
+		count = arr.length;
+		for (i = 0; i < count; i++) {
+			data = arr[i];
+			if (data === null) continue;
+			
+			if (data[0] === targetId) {
+				arr.splice(i, 1, [targetId, value])
+				break;
+			}
+		}
+		if (i === count) {
+			arr.push([targetId, value]);
+		}
+		
+		this.setCustomParameter(arr);
+		
+//		this._putLog(arr);
+	},
+	
+	_putLog: function(arr) {
+		var unit = this._getUnit();
+		var keyword = this._getKeyWord();
+		var count = arr.length;
+		var i;
+		
+		if (unit === null || keyword === '') {
+			root.log('unit または keyword が不正');
+			return;
+		}
+		if (count === 0) {
+			root.log('unitのカスタムパラメータ ' + keyword +' が不正');
+		}
+		
+		for (i = 0; i < count; i++) {
+			root.log(unit.getName() + ' targetId:' + arr[i][0] + ' value:' + arr[i][1])
+		}
+	}
+	
+};
