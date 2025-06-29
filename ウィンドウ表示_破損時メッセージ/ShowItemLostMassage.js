b9524ed430d5828c8dfcedb5d2a36833c0adc2e5
RantaroGames <96756070+RantaroGames@users.noreply.github.com>
2025-06-30 03:09:33 +0900

    Create ShowItemLostMassage.js

---
+++ ウィンドウ表示_破損時メッセージ/ShowItemLostMassage.js
@@ -0,0 +1,201 @@
+/*
+■ファイル名
+ShowItemLostMassage.js
+
+■SRPG Studio対応バージョン
+ver.1.315
+
+■プラグインの概要
+アイテム破損時にメッセージを表示する処理を追加します。
+例えば、杖タイプのアイテムが壊れた時に破損時メッセージを表示できるようになります。
+
+■使用方法
+1.このプラグインをpluginフォルダに入れる
+
+2.「アイテム」または、コンフィグで設定する「武器タイプ」のカスタムパラメータに以下のように記述する
+{
+  showItemLostMessage: true
+}
+
+■作成者
+ran
+
+■利用規約
+https://github.com/RantaroGames/SRPG_Studio/blob/be1b84ab349a0ac1a3573bf645e5c78cb3ab12c3/README.md
+
+■更新履歴
+2025/06/30 新規作成
+
+*/
+
+(function() {
+
+//---------------------------------------------------------------------
+// 「'が壊れました'」を'任意の文字'(''で文字を囲むこと)にすると表記を変えることができる
+//---------------------------------------------------------------------
+StringTable.ItemLost = 'が壊れました';
+
+//---------------------------------------------------------------------------
+// 破損したアイテムの名前を表示する(メッセージタイトルで画面中央に表示)処理
+//---------------------------------------------------------------------------
+var ItemLostMessageControl = {
+	showMassageTitle: function(unit, item, generator) {
+		generator.soundPlay(this._getLostSoundHandle(), 1);
+		generator.messageTitle(item.getName() + StringTable.ItemLost, 0, 0, true);
+	},
+
+	_isItemLostDisplayable: function(unit, item) {
+		if (item === null) return false;	
+		
+		// コンフィグで「武器破損時にメッセージを表示する」にチェックが入っていない場合は表示しない
+		if (!DataConfig.isWeaponLostDisplayable()) return false;
+		
+		// スキップモードの時は表示しない
+		if (CurrentMap.isCompleteSkipMode()) return false;
+				
+		// プレイヤーユニット以外は表示しない
+		if (unit.getUnitType() !== UnitType.PLAYER) return false;
+
+		// アイテムまたはアイテムタイプのカスタムパラメータにtrueを設定していれば表示する
+		if (item.custom.showItemLostMessage === true || item.getWeaponType().custom.showItemLostMessage === true) {
+			return true;
+		}
+		
+		return false;
+	},
+	
+	_getLostSoundHandle: function() {
+		return root.querySoundHandle('itemlost');
+	}
+};
+
+//---------------------------------------------------------------------------
+// 使用したアイテムが破損していたらメッセージを表示する処理を追加する
+//---------------------------------------------------------------------------
+var _ItemUseParent__pushFlowEntries = ItemUseParent._pushFlowEntries;
+ItemUseParent._pushFlowEntries = function(straightFlow) {
+	_ItemUseParent__pushFlowEntries.call(this, straightFlow);
+	
+	straightFlow.pushFlowEntry(ItemBrokenFlowEntry);
+};
+
+	
+var ItemBrokenFlowEntry = defineObject(BaseFlowEntry,
+{	
+	_itemUseParent: null,
+	_dynamicEvent: null,
+	
+	enterFlowEntry: function(itemUseParent) {
+		this._prepareMemberData(itemUseParent);
+		return this._completeMemberData(itemUseParent);
+	},
+	
+	moveFlowEntry: function() {
+		if (this._itemUseParent.isItemSkipMode()) {
+			return MoveResult.END;
+		}
+		
+		return this._dynamicEvent.moveDynamicEvent();
+	},
+	
+	_prepareMemberData: function(itemUseParent) {
+		this._itemUseParent = itemUseParent;
+		this._dynamicEvent = createObject(DynamicEvent);
+	},
+	
+	_completeMemberData: function(itemUseParent) {
+		var generator;
+		var itemTargetInfo = this._itemUseParent.getItemTargetInfo();
+		var unit = itemTargetInfo.unit;
+		var item = itemTargetInfo.item;
+		var baseItem = root.getBaseData().getItemList().getDataFromId(item.getId());
+		
+		// itemUseParent.decreaseItem()が実行されてアイテムが破損した時、「破損時アイテム」が設定されていると
+		// itemTargetInfo.itemが置換されてカスタムパラメータが正常に取得できないのでベースデータから取得したアイテムで判定している
+		if (!ItemLostMessageControl._isItemLostDisplayable(unit, baseItem)) {
+			return EnterResult.NOTENTER;
+		}
+		
+		// 本来、アイテムの使用回数を減らす処理はItemUseParentのstraightFlowが全て終わった（またはフローに入らなかった）際に実行される
+		// アイテム破損時のメッセージ表示をstraightFlowに追加したので、このタイミングで使用回数を減らす処理を入れる
+		if (!this._itemUseParent._isItemDecrementDisabled) {
+			this._itemUseParent.decreaseItem();
+			
+			// アイテムを減らしたのでその旨を記録しておく
+			this._itemUseParent.disableItemDecrement();
+		}
+		
+		// 「耐久無限でないアイテムの耐久が0である または 破損状態が設定されている」なら破損時メッセージを表示する
+		if (ItemControl.isItemBroken(item) || item.getLimit() === WeaponLimitValue.BROKEN) {
+			generator = this._dynamicEvent.acquireEventGenerator();
+			
+			ItemLostMessageControl.showMassageTitle(unit, baseItem, generator);
+			
+			return this._dynamicEvent.executeDynamicEvent();
+		}
+		
+		return EnterResult.NOTENTER;
+	}
+}
+);
+
+
+//---------------------------------------------------------------------------
+// ユニットコマンド「扉、宝箱」経由で鍵アイテムを使用した際に破損時のメッセージ処理を追加する
+//---------------------------------------------------------------------------
+var _KeyNavigator__pushFlowEntries = KeyNavigator._pushFlowEntries;
+KeyNavigator._pushFlowEntries = function(straightFlow) {
+	_KeyNavigator__pushFlowEntries.call(this, straightFlow);
+	
+	straightFlow.pushFlowEntry(ItemBrokenFlowEntry_KeyNavigator);
+};
+
+var ItemBrokenFlowEntry_KeyNavigator = defineObject(BaseFlowEntry,
+{	
+	_keyNavigator: null,
+	_dynamicEvent: null,
+	
+	enterFlowEntry: function(keyNavigator) {
+		this._prepareMemberData(keyNavigator);
+		return this._completeMemberData(keyNavigator);
+	},
+	
+	moveFlowEntry: function() {
+		if (CurrentMap.isCompleteSkipMode()) {
+			return MoveResult.END;
+		}
+		
+		return this._dynamicEvent.moveDynamicEvent();
+	},
+	
+	_prepareMemberData: function(keyNavigator) {
+		this._keyNavigator = keyNavigator;
+		this._dynamicEvent = createObject(DynamicEvent);
+	},
+	
+	_completeMemberData: function(keyNavigator) {
+		var generator;
+		var unit = this._keyNavigator.getUnit();
+		var item = this._keyNavigator.getKeyData().item;
+		var baseItem = root.getBaseData().getItemList().getDataFromId(item.getId());
+		
+		// 鍵アイテムをユニットコマンド経由で使用する場合は、KeyTrophyFlowEntryで鍵の耐久を減らす処理が実行されている
+		// 「破損時アイテム」が設定されているとKeyDataのitemが置換されてカスタムパラメータが正常に取得できないのでベースデータから取得したアイテムで判定している
+		if (!ItemLostMessageControl._isItemLostDisplayable(unit, baseItem)) {
+			return EnterResult.NOTENTER;
+		}
+
+		if (ItemControl.isItemBroken(item) || item.getLimit() === WeaponLimitValue.BROKEN) {
+			generator = this._dynamicEvent.acquireEventGenerator();
+			
+			ItemLostMessageControl.showMassageTitle(unit, baseItem, generator);
+			
+			return this._dynamicEvent.executeDynamicEvent();
+		}
+
+		return EnterResult.NOTENTER;
+	}
+}
+);
+
+})();
